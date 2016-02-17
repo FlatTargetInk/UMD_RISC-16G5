072595efeb5bc46fd2062f41a704be18d13d5d76
Robert M <flattargetink@gmail.com>
2016-02-17 10:25:07 -0500

    Added Machine Diagram

--- Lab2/Code/RPNCalc/RPN_toplevel.vhd
+++ Lab2/Code/RPNCalc/RPN_toplevel.vhd
@@ -1,6 +1,7 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- School:		University of Massachusetts Dartmouth
+-- Department:	Electrical and Computer Engineering
+-- Engineers:	
 -- 
 -- Create Date:    16:43:51 02/12/2016 
 -- Design Name: 
@@ -24,13 +25,13 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use work.all;
 
 entity RPN_toplevel is
-	Port (	ADDB		: in STD_LOGIC;
+	Port (ADDB		: in STD_LOGIC;
 			SUBB		: in STD_LOGIC;
-			ENTB	    : in STD_LOGIC;
+			ENTB	   : in STD_LOGIC;
 			CLRB    	: in STD_LOGIC;
 			NOIN    	: in  STD_LOGIC_VECTOR (7 downto 0);
-			NOOUTA		: out STD_LOGIC_VECTOR (7 downto 0);
-			NOOUTB		: out STD_LOGIC_VECTOR (7 downto 0);
+			NOOUTA	: out STD_LOGIC_VECTOR (7 downto 0);
+			NOOUTB	: out STD_LOGIC_VECTOR (7 downto 0);
 			OP			: out STD_LOGIC_VECTOR (3 downto 0));
 end RPN_toplevel;
 
@@ -41,24 +42,24 @@ architecture Behavioral of RPN_toplevel is
 
 begin
 	Add_unit: Entity work.Add_unit
-	port map(	INDATA	=>	NOIN,
-				EN		=>	ADDB,
+	port map(INDATA	=>	NOIN,
+				EN			=>	ADDB,
 				OUTDATA	=>	NOOUTB,
 				CMD		=>	OP);
 				
 	Sub_unit: Entity work.Sub_unit
-	port map(	INDATA	=> NOIN,
-				EN		=> SUBB,
+	port map(INDATA	=> NOIN,
+				EN			=> SUBB,
 				OUTDATA	=> NOOUTB,
 				CMD		=> OP);
 				
 	Stack_unit: Entity work.Stack_unit
-	port map(	INDATA	=> NOIN,
-				EN		=> ENTB,
+	port map(INDATA	=> NOIN,
+				EN			=> ENTB,
 				OUTDATA	=> NOOUTA);
 	
 	Clear_unit: Entity work.Clear_unit
-	port map(	EN		=> CLRB,
+	port map(EN		=> CLRB,
 				CLEAR	=> NOOUTA,
 				CLEAR	=> NOOUTB);	-- can I do this?
 				
