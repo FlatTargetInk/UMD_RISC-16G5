8352053c4ce125653891b05feef48de07e14d1b1
treej314 <tjdoucette314@gmail.com>
2016-04-28 21:00:37 -0400

    HardwareDebugDemo

---
+++ ProjectLab2/HardwareDebugDemo/Hardware_TL.vhd
@@ -0,0 +1,125 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:26:43 04/22/2016 
+-- Design Name: 
+-- Module Name:    Hardware_TL - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use work.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Hardware_TL is
+	Port(	CLK	: in STD_LOGIC;
+			BTN	: in STD_LOGIC_VECTOR(3 downto 0);
+			SW		: in STD_LOGIC_VECTOR(7 downto 0);
+			LED	: out STD_LOGIC_VECTOR(7 downto 0);
+			SEG	: out STD_LOGIC_VECTOR(6 downto 0);
+			DP		: out STD_LOGIC;
+			AN		: out	STD_LOGIC_VECTOR(0 to 3);
+			JA		: out STD_LOGIC_VECTOR(7 downto 0);
+			JB		: out STD_LOGIC_VECTOR(7 downto 0);
+			JC		: out STD_LOGIC_VECTOR(7 downto 0);
+			JD		: out STD_LOGIC_VECTOR(3 downto 0));
+end Hardware_TL;
+
+architecture Structural of Hardware_TL is
+
+----> Management <----
+signal HW_EN			: STD_LOGIC := '1';	-- Hardware Enable Line
+signal HW_RST			: STD_LOGIC := '0';	-- Hardware Reset Line
+----> Inputs <----
+signal btn_sig			: STD_LOGIC_VECTOR (3 downto 0) := (OTHERS => '0');
+signal software_clk	: STD_LOGIC := '0';
+signal sft_rst			: STD_LOGIC := '0';
+----> Outputs <----
+signal SSEG_DATA 		: STD_LOGIC_VECTOR (15 downto 0) := X"0000";	-- Debug with Seven Segment Display
+signal DBUG_BUS		: STD_LOGIC_VECTOR (15 downto 0) := (OTHERS => '0');
+signal ALU_OUT			: STD_LOGIC_VECTOR (15 downto 0) := (OTHERS => '0');
+signal DEBUG_DATA		: STD_LOGIC_VECTOR (15 downto 0)	:= (OTHERS => '0');
+
+begin
+	
+	SFT_RST <= btn_sig(0);
+--------  Place UUT Here  --------
+----------------------------------
+	JA <= SW;
+	JB <= SW;
+	JC <= SW;
+	JD <= SW(3 downto 0);
+	LED <= (OTHERS => '0');
+--	UUT: entity work.ProjLab01
+--	Port map(CLK 		=> software_clk,
+--				RST 		=> SFT_RST,
+--				ALU_OUT	=> ALU_OUT,
+--				DST_ADR	=> DBUG_BUS,
+--				DEBUG_OUT	=> DEBUG_DATA);
+--				STORE_DATA : out  STD_LOGIC_VECTOR (15 downto 0);
+--				CCR : out  STD_LOGIC_VECTOR (3 downto 0));
+	
+	----> Mappings <----
+--	LED <= DBUG_BUS(7 downto 0);
+--	JA <= (OTHERS => '0');
+--	JB <= (OTHERS => '1');
+--	JC <= (OTHERS => '0');
+--	JD <= (OTHERS => '1');
+
+--------  Hardware Testing Devices  --------
+--------------------------------------------
+
+	----> Output Selector <----
+	with SW(0) select SSEG_DATA <=
+		ALU_OUT when '0',
+		DEBUG_DATA when '1',
+		ALU_OUT when OTHERS;
+		
+	----> Seven Segment Output <----
+	SSeg_unit: entity work.SSeg_toplevel
+	port map(CLK 	=> CLK,
+				DATA	=> SSEG_DATA,
+				RST 	=> HW_RST,
+				SEG 	=> SEG,
+				DP  	=> DP,
+				AN  	=> AN);
+
+	----> Button Input Controller <----
+	Buttons: entity work.buttoncontrol
+	port map(CLK	=> CLK,
+				EN    => HW_EN,
+				BTN  	=> BTN,
+				LED 	=> btn_sig);
+	
+	----> Clock Generator <----
+	ClkGen: entity work.clock_toplevel
+	port map(CLK 	=> CLK,
+				BTN 	=> btn_sig(3),
+				SW  	=> SW(7 downto 6),
+				SWCLK	=> software_clk);
+
+
+
+end Structural;
+
---
+++ ProjectLab2/HardwareDebugDemo/SevenSeg_toplevel.vhd
@@ -0,0 +1,69 @@
+---------------------------------------------------
+-- School:     University of Massachusetts Dartmouth
+-- Department: Computer and Electrical Engineering
+-- Engineer:   Daniel Noyes
+-- 
+-- Create Date:    SPRING 2015
+-- Module Name:    SevenSeg_toplevel
+-- Project Name:   SevenSegmentDisplay
+-- Target Devices: Spartan-3E
+-- Tool versions:  Xilinx ISE 14.7
+--
+-- Description: 7-segment toplevel example
+---------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use work.all;
+
+entity SSeg_toplevel is
+
+    port (
+        CLK : in  STD_LOGIC; -- 50 MHz input
+        DATA: in  STD_LOGIC_VECTOR (15 downto 0);
+        RST : in  STD_LOGIC;
+        SEG : out STD_LOGIC_VECTOR (6 downto 0);
+        DP  : out STD_LOGIC;
+        AN  : out STD_LOGIC_VECTOR (3 downto 0)
+    );
+
+end SSeg_toplevel;
+
+architecture Structural of SSeg_toplevel is
+
+--  signal s0	: STD_LOGIC_VECTOR (3 downto 0)	:= "0000";
+--	 signal s1	: STD_LOGIC_VECTOR (3 downto 0)	:= X"0";
+--	 signal s2  : STD_LOGIC_VECTOR (3 downto 0) := "0000";
+--  signal s3  : STD_LOGIC_VECTOR (3 downto 0) := "0000";
+    signal enl : STD_LOGIC := '1';
+    signal dpc : STD_LOGIC_VECTOR (3 downto 0) := "1111";
+    signal cen : STD_LOGIC := '0';
+
+begin
+
+    ----- Structural Components: -----
+    SSeg: entity work.SSegDriver
+    port map( CLK     => CLK,
+              RST     => RST,
+              EN      => enl,
+              SEG_0   => DATA(3 downto 0),
+              SEG_1   => DATA(7 downto 4),
+              SEG_2   => DATA(11 downto 8),
+              SEG_3   => DATA(15 downto 12),
+              DP_CTRL => dpc,
+              COL_EN  => cen,
+              SEG_OUT => SEG,
+              DP_OUT  => DP,
+              AN_OUT  => AN);
+
+    
+    ----- End Structural Components -----
+
+end Structural;
+
+
+
+
+
+
